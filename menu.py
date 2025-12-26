85b6f43fdebd7ed778cda24d4753bf46f7963305
yanis <yanis.hamouda@efrei.net>
2025-12-26 16:15:41 +0100

    Chapitre 4

--- menu.py
+++ menu.py
@@ -2,6 +2,7 @@ from utils.input_utils import demander_texte
 from chapitres.chapitre_1 import lancer_chapitre_1
 from chapitres.chapitre_2 import lancer_chapitre_2
 from chapitres.chapitre_3 import lancer_chapitre_3
+from chapitres.chapitre_4 import lancer_chapitre_4
 
 
 
@@ -9,7 +10,7 @@ def afficher_menu_principal():
     print("\n" + "="*40)
     print("🏰 MENU PRINCIPAL POUDELARD")
     print("="*40)
-    print("1. Lancer l'aventure (Chapitres 1 à 3)")
+    print("1. Lancer l'aventure (Chapitres 1 à 4)")
     print("2. Quitter le jeu")
     print("="*40)
 
@@ -36,6 +37,12 @@ def lancer_choix_menu():
 
             input("\n✨ Appuyez sur Entrée pour commencer les cours (Chapitre 3)...")
             lancer_chapitre_3(joueur, maisons)
+
+            input("\n🧹 Appuyez sur Entrée pour commencer le Grand Match de Quidditch (Chapitre 4)... ")
+            lancer_chapitre_4(joueur, maisons)
+
+            print("\n🏆 Félicitations ! Vous avez terminé l'aventure.")
+            print("Merci d'avoir joué au Projet Poudelard.")
 
         elif choix == "2":
             print("Au revoir et a bientôt dans le monde des sorciers !")
